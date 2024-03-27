763e541f21856a6541381e19083549d8dd209130
YAndrewk <yandrewkg@gmail.com>
2024-03-27 16:33:32 +0300

    2nd commit

--- Chrome.py
+++ Chrome.py
@@ -5,16 +5,16 @@ from selenium import webdriver
 print ('hello')
 
 print ('hello from Dima')
-
+executable_path="C:\\Users\\Андрей\\AppData\\Local\\Programs\\Opera\\operadriver.exe"
 from selenium.webdriver import Opera, OperaOptions
-Set the path to the Opera driver executable
-executable_path = ‘/path/to/operadriver’
-Set the options for the Opera browser
+#Set the path to the Opera driver executable
+#executable_path = ‘/path/to/operadriver’
+#Set the options for the Opera browser
 options = OperaOptions()
-options.add_argument(‘–disable-gpu’)
-options.add_argument(‘–no-sandbox’)
-Create a new instance of the Opera driver
-driver = Opera(executable_path=executable_path, options=options)
+options.add_argument("–disable-gpu")
+options.add_argument("–no-sandbox")
+#Create a new instance of the Opera driver
+driver = Opera(executable_path,options)
 
 browser = webdriver.Opera('C:/Users/Андрей/AppData/Local/Programs/Opera')
 #browser = webdriver.Opera(options=options, executable_path=r'location_of_operadriver.exe'
